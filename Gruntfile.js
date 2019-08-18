891f88e55b66488c68aa126ccd61b98e80dbc581
Simon <watsonsemon@gmail.com>
2019-08-19 10:49:36 +1200

    JS added

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,15 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         ///////////////////////////////////////////////
-
+        jshint: {
+            all: ['js/*.js'],
+            options: {
+                'esversion': 6,
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
+    grunt.registerTask('js', ['jshint']);
 };
