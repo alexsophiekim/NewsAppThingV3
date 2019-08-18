d13bd7c0bcaf66fb56a6f079cc7ef74f1d8c7c26
Sophie Kim <270010565@bun2-206-17.local>
2019-08-19 09:51:56 +1200

    edited

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,7 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      watch:{
-         sass: {
-           files: ['scss/*.scss'],
-           tasks: ['sass', 'cssmin']
-         },
-         js: {
-           files: ['js/*.js', '!js/*.min.js'],
-           tasks: ['jshint', 'uglify']
-         }
-       },
+
     });
-
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-
-
-    grunt.registerTask('default', ['watch']);
-
 
 
 };
