189353dd4c307f90798744631409901fd72cd287
Sophie Kim <270010565@bun2-206-17.local>
2019-08-19 10:53:59 +1200

    trying to install gruntfiles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,16 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+      jshint: {
+        files: ['*/*.js','js/script.js'],
+        options:{
+          globals:{
+            jQuery:true
+          }
+        }
+      },
+
+      ///////////
+
 
     });
-
-
 };
